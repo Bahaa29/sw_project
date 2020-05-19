d578638fcad3d1cf4909dfa5b55ce90bd14ebc81
root <root@localhost.localdomain>
2020-05-19 05:08:25 +0200

    2

---
+++ src/GoFo/Admin.java
@@ -0,0 +1,17 @@
+package GoFo;
+
+public class Admin
+{
+    public void Approve_playground(string name)
+    {
+
+    }
+    public void suspend_playground(string name)
+    {
+
+    }
+    public void delete_playground(string name)
+    {
+        
+    }
+}
