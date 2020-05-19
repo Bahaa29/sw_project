aa2044f34670625c9d2a444d153df315e6278f03
Mohamed Mahmoud <mohamedmahmoud6524@gmail.com>
2020-05-18 23:17:15 -0400

    Player class

---
+++ src/GoFo/Player.java
@@ -0,0 +1,42 @@
+package GOFO;
+
+public class Player {
+    private String[] my_bookings = new String[20];
+    public void book_playground(String playground_name, String hours, String date)
+    {
+
+    }
+    public String display_nearest_playground()
+    {
+
+    }
+    public String display_playground(String location)
+    {
+
+    }
+    public String display_nearest_playground(String date, String hours)
+    {
+
+    }
+    public boolean check_available_timeslot(String timeslot)
+    {
+
+    }
+    public void pay(float amount)
+    {
+
+    }
+    public void send_invitation(String player_email)
+    {
+
+    }
+    public void cancel_booking(String playground_name, String date)
+    {
+
+    }
+    void show_all_playgrounds()
+    {
+
+    }
+
+}
