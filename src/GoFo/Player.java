d22cca6da4b89bdca465503972d7b460c3d4905c
Bahaa <bahaaeldin390@gmail.com>
2020-05-20 08:02:40 +0200

    second edit

--- src/GoFo/Player.java
+++ src/GoFo/Player.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 
 public class Player extends User {
 
-    public void book_playground(Vector<Booking_Struct> ADD)
+    public void book_playground(Vector<Booking_Struct> ADD,Vector<Playground> All,Vector<User> all_users)
     {
         Booking_Struct obj=new Booking_Struct();
         Scanner scan=new Scanner(System.in);
@@ -25,15 +25,33 @@ public class Player extends User {
         System.out.println("enter the playground_owner_name");
         String playground_owner_name =scan.nextLine();
         obj.setPlayground_owner(playground_owner_name);
+        float total=0;
+        for (int i=0;i<All.size();i++)
+        {
+            if(All.elementAt(i).getName().equalsIgnoreCase(playground_name))
+            {
+                total=All.elementAt(i).getPrice_per_hour();
+            }
+        }
+        total*=hour_book;
+        obj.setTotal(total);
         for(int i=0;i<ADD.size();i++)
         {
             if(ADD.size()==0)
             {
+                pay(total,all_users,name,playground_owner_name);
                 ADD.add(obj);
             }
             else
             {
-
+                boolean flag=check_available_timeslot(date,ADD,All,playground_owner_name,playground_name,hour_book);
+                if(flag)
+                {
+                    pay(total,all_users,name,playground_owner_name);
+                    ADD.add(obj);
+                }
+                else
+                    System.out.println("u cant booking in this day for this playground");
             }
         }
     }
@@ -76,9 +94,33 @@ public class Player extends User {
             return  true;
 
     }
-    public void pay(float amount)
+    public boolean pay(float amount,Vector<User> all_users,String player_name,String owner_Name)
     {
+        float price=0;
+        boolean flag=false;
+        for(int i=0;i<all_users.size();i++)
+        {
+            if(all_users.elementAt(i).getName().equalsIgnoreCase(player_name))
+            {
+                price=all_users.elementAt(i).wallet.getBalance();
+                if(price>=amount) {
+                    System.out.println("ok");
+                    all_users.elementAt(i).wallet.setBalance(price-amount);
+                    flag=true;
+                }
+                else
+                    flag=false;
+            }
+        }
+        for(int i=0;i<all_users.size();i++)
+        {
+            if(all_users.elementAt(i).getName().equalsIgnoreCase(owner_Name))
+            {
+                all_users.elementAt(i).wallet.setBalance(all_users.elementAt(i).wallet.getBalance()+amount);
+            }
 
+        }
+        return flag;
     }
     public void send_invitation(String player_email)
     {
@@ -88,9 +130,12 @@ public class Player extends User {
     {
 
     }
-    public void show_all_playgrounds()
+    public void show_all_playgrounds(Vector<Playground> All)
     {
-
+        for(int i=0;i<All.size();i++)
+        {
+            System.out.println(All.elementAt(i).getName()+"-");
+        }
     }
 
 }
